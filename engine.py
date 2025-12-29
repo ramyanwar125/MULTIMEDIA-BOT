52e7eac1c62d8a65263dcc2f059775fbbe942a0a
ramyanwar125 <ramyanwar880@gmail.com>
2025-12-29 13:34:11 +0200

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,39 +1,37 @@
+import os
 import yt_dlp
 
-def get_all_formats(url):
-    """جلب الجودات المتاحة للفيديو"""
-    ydl_opts = {
-        'quiet': True,
-        'no_warnings': True,
-        'format': 'best',
-    }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        try:
-            info = ydl.extract_info(url, download=False)
-            formats = info.get('formats', [])
-            res_dict = {}
-            
-            for f in formats:
-                # نختار الملفات التي تحتوي على صوت وفيديو معاً لسهولة التحميل
-                if f.get('vcodec') != 'none' and f.get('acodec') != 'none':
-                    res = f.get('height')
-                    if res:
-                        res_name = f"{res}p"
-                        res_dict[res_name] = f.get('format_id')
-            
-            # ترتيب الجودات من الأعلى للأقل
-            return dict(sorted(res_dict.items(), key=lambda x: int(x[0].replace('p','')), reverse=True))
-        except Exception as e:
-            print(f"Error in engine: {e}")
-            return None
+def prepare_engine():
+    cookie_file = "cookies_stable.txt"
+    if not os.path.exists(cookie_file):
+        with open(cookie_file, "w") as f:
+            f.write("# Netscape HTTP Cookie File\n")
+            f.write(".youtube.com\tTRUE\t/\tTRUE\t1766757959\tGPS\t1\n")
+            f.write(".youtube.com\tTRUE\t/\tTRUE\t1801316163\tPREF\ttz=Africa.Cairo&f7=100\n")
+            f.write(".youtube.com\tTRUE\t/\tTRUE\t1800424038\tSOCS\tCAISEwgDEgk4NDYxMjU0NDcaAmVuIAEaBgiA8ZzKBg\n")
+    return cookie_file
 
-def run_download(url, format_id, output_path):
-    """تنفيذ عملية التحميل الفعلي"""
+def get_all_formats(url):
+    ydl_opts = {'quiet': True, 'cookiefile': prepare_engine(), 'nocheckcertificate': True, 'no_warnings': True}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        formats_btns = {}
+        for f in info.get('formats', []):
+            if f.get('vcodec') != 'none' and f.get('acodec') != 'none' and f.get('ext') == 'mp4':
+                res = f.get('height')
+                if res: formats_btns[f"🎬 {res}p"] = f.get('format_id')
+        formats_btns["🎶 Audio | تحميل صوت"] = "bestaudio[ext=m4a]/bestaudio"
+        return formats_btns
+
+def run_download(url, format_id, file_path):
     ydl_opts = {
+        'outtmpl': file_path,
         'format': format_id,
-        'outtmpl': output_path,
+        'cookiefile': prepare_engine(),
+        'nocheckcertificate': True,
         'quiet': True,
-        'no_warnings': True,
+        'concurrent_fragment_downloads': 15,
+        'continuedl': True,
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
