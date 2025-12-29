e380b72fac3d57cf03b1dcb06b625eb2d9418955
ramyanwar125 <ramyanwar880@gmail.com>
2025-12-29 16:30:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from engine import get_all_formats, run_download
 # --- Config | الإعدادات ---
 API_ID = 33536164
 API_HASH = "c4f81cfa1dc011bcf66c6a4a58560fd2"
-BOT_TOKEN = "8320774023:AAGgwvYIXX8s3R1NFvAGDeooYyPkjBJ_-1E"
+BOT_TOKEN = "8320774023:AAEhtKqGmayEAwRNz9K8ou48WlRyDoqL_pk"
 ADMIN_ID = 7349033289 
 DEV_USER = "@TOP_1UP"
 BOT_NAME = "『 ＦＡＳＴ ＭＥＤＩＡ 』"
