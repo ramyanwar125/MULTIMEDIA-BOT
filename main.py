2a48db0cb0962e0b1ee4c24a18b1270aa402f408
ramyanwar125 <ramyanwar880@gmail.com>
2025-12-30 21:12:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def run_web():
 API_ID = 33536164
 API_HASH = "c4f81cfa1dc011bcf66c6a4a58560fd2"
 # ملاحظة: تأكد أن التوكن أدناه هو الأحدث من BotFather
-BOT_TOKEN = "8304738811:AAHCUi_i6F9j5WWkkn_muwDn-kAx2nOePl0"
+BOT_TOKEN = "8304738811:AAESGIvrYoDYoRjb7hZZURcj-rYTxVGAsYw"
 ADMIN_ID = 7349033289 
 DEV_USER = "@TOP_1UP"
 BOT_NAME = "『 ＦＡＳＴ ＭＥＤＩＡ 』"
