7eb3e80911a654af55d44a0186a6deec98625bcd
ramyanwar125 <ramyanwar880@gmail.com>
2025-12-31 13:08:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def run_health_check_server():
 # --- Config | الإعدادات ---
 API_ID = 33536164
 API_HASH = "c4f81cfa1dc011bcf66c6a4a58560fd2"
-BOT_TOKEN = "8151240519:AAGqX7jZpknP7ZTqQc2saY-H7KjuTi2tWWg"
+BOT_TOKEN = "8151240519:AAFMdgf3Pi37HL0JtuH3MB2uFU63ZayrVmc"
 ADMIN_ID = 7349033289 
 DEV_USER = "@TOP_1UP"
 BOT_NAME = "『 ＦＡＳＴ ＭＥＤＩＡ 』"
